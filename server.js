5230fd42c50f70e50624ecfd2c5cabab20c01d12
AjayChitumalla <chitumallaajay209@gmail.com>
2021-03-16 16:42:48 +0530

    1st deploy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/architectui-angular-free'));
 app.listen(process.env.PORT || 4200);
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/architectui-angular-free/index.html'));
 })
 console.log('listening');
